f1899fe4c6b354936e79437665b96161d322e0f0
jasonqiu212 <jason_qiu@hotmail.com>
2023-01-31 01:53:25 +0800

    Add support requests table to support requests page

--- src/web/app/pages-admin/admin-support-requests-page/admin-support-requests-page.component.ts
+++ src/web/app/pages-admin/admin-support-requests-page/admin-support-requests-page.component.ts
@@ -1,42 +1,155 @@
-import { Component } from '@angular/core';
-// import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-// import { forkJoin, Observable, of, throwError } from 'rxjs';
-// import { catchError, finalize, map, mergeMap } from 'rxjs/operators';
-// import { AccountService } from '../../../services/account.service';
-// import { CourseService } from '../../../services/course.service';
-// import { LinkService } from '../../../services/link.service';
-// import { SimpleModalService } from '../../../services/simple-modal.service';
-// import { StatusMessageService } from '../../../services/status-message.service';
-// import { Account, Accounts, Courses, JoinLink } from '../../../types/api-output';
-// import { SimpleModalType } from '../../components/simple-modal/simple-modal-type';
-// import { ErrorMessageOutput } from '../../error-message-output';
+import { Component } from "@angular/core";
+import { StatusMessageService } from "src/web/services/status-message.service";
+import { SupportRequestService } from "../../../services/support-request.service";
+import { TableComparatorService } from "../../../services/table-comparator.service";
+import { SupportRequest, SupportRequests } from "../../../types/api-output";
+import { SortBy, SortOrder } from "../../../types/sort-properties";
+import {
+  SupportRequestsTableColumn,
+  SupportRequestsTableRowModel,
+} from "../../components/support-requests-table/support-requests-table-model";
+import { ErrorMessageOutput } from "../../error-message-output";
+
+/**
+ * Data model for the support requests tab.
+ */
+export interface SupportRequestsTabModel {
+  supportRequestsTableRowModels: SupportRequestsTableRowModel[];
+  supportRequestsTableRowModelsSortBy: SortBy;
+  supportRequestsTableRowModelsSortOrder: SortOrder;
+
+  hasPopulated: boolean;
+  isAjaxSuccess: boolean;
+  hasLoadingFailed: boolean;
+}
 
 /**
  * Admin support requests page.
  */
 @Component({
-  selector: 'tm-admin-support-requests-page',
-  templateUrl: './admin-support-requests-page.component.html',
-  styleUrls: ['./admin-support-requests-page.component.scss'],
+  selector: "tm-admin-support-requests-page",
+  templateUrl: "./admin-support-requests-page.component.html",
+  styleUrls: ["./admin-support-requests-page.component.scss"],
 })
 export class AdminSupportRequestsPageComponent {
+  // enum
+  SupportRequestsTableColumn: typeof SupportRequestsTableColumn =
+    SupportRequestsTableColumn;
+  SortBy: typeof SortBy = SortBy;
 
-//   instructorDetails: string = '';
-//   instructorName: string = '';
-//   instructorEmail: string = '';
-//   instructorInstitution: string = '';
+  // data
+  supportRequestsTabModel: SupportRequestsTabModel = {
+    supportRequestsTableRowModels: [],
+    supportRequestsTableRowModelsSortBy: SortBy.SUPPORT_REQUEST_CREATE_TIME,
+    supportRequestsTableRowModelsSortOrder: SortOrder.ASC,
+    hasPopulated: false,
+    isAjaxSuccess: false,
+    hasLoadingFailed: false,
+  };
 
-//   instructorsConsolidated: InstructorData[] = [];
-//   activeRequests: number = 0;
+  constructor(
+    private tableComparatorService: TableComparatorService,
+    private supportRequestService: SupportRequestService,
+    private statusMessageService: StatusMessageService
+  ) {}
 
-//   isAddingInstructors: boolean = false;
+  ngOnInit(): void {
+    this.loadSupportRequests();
+  }
 
-//   isRegisteredInstructorModalLoading = false;
-//   registeredInstructorIndex: number = 0;
-//   registeredInstructorAccountData: RegisteredInstructorAccountData[] = [];
+  /**
+   * Loads support requests.
+   */
+  loadSupportRequests(): void {
+    this.supportRequestsTabModel = {
+      supportRequestsTableRowModels: [],
+      supportRequestsTableRowModelsSortBy: SortBy.SUPPORT_REQUEST_CREATE_TIME,
+      supportRequestsTableRowModelsSortOrder: SortOrder.ASC,
+      hasPopulated: false,
+      isAjaxSuccess: false,
+      hasLoadingFailed: false,
+    };
+    this.supportRequestService
+      .getSupportRequestsForEmail("alice@abc.com")
+      .subscribe({
+        next: (response: SupportRequests) => {
+          response.supportRequests.forEach((supportRequest: SupportRequest) => {
+            const m: SupportRequestsTableRowModel = {
+              supportRequest,
+            };
+            this.supportRequestsTabModel.supportRequestsTableRowModels.push(m);
+          });
+          this.supportRequestsTabModel.hasPopulated = true;
+          if (!this.supportRequestsTabModel.isAjaxSuccess) {
+            this.supportRequestsTabModel.isAjaxSuccess = true;
+          }
+        },
+        error: (resp: ErrorMessageOutput) => {
+          this.supportRequestsTabModel.hasLoadingFailed = true;
+          this.statusMessageService.showErrorToast(resp.error.message);
+        },
+      });
+  }
 
-//   @ViewChild('registeredInstructorModal') registeredInstructorModal!: TemplateRef<any>;
+  /**
+   * Sorts the list of support request row.
+   */
+  sortSupportRequestsTableRowModelsEvent(by: SortBy): void {
+    this.supportRequestsTabModel.supportRequestsTableRowModelsSortBy = by;
 
-  constructor() {}
+    // reverse the sort order
+    this.supportRequestsTabModel.supportRequestsTableRowModelsSortOrder =
+      this.supportRequestsTabModel.supportRequestsTableRowModelsSortOrder ===
+      SortOrder.DESC
+        ? SortOrder.ASC
+        : SortOrder.DESC;
 
+    this.supportRequestsTabModel.supportRequestsTableRowModels.sort(
+      this.sortModelsBy(
+        by,
+        this.supportRequestsTabModel.supportRequestsTableRowModelsSortOrder
+      )
+    );
+  }
+
+  /**
+   * Generates a sorting function.
+   */
+  protected sortModelsBy(
+    by: SortBy,
+    order: SortOrder
+  ): (
+    a: { supportRequest: SupportRequest },
+    b: { supportRequest: SupportRequest }
+  ) => number {
+    return (
+      a: { supportRequest: SupportRequest },
+      b: { supportRequest: SupportRequest }
+    ): number => {
+      let strA: string;
+      let strB: string;
+      switch (by) {
+        case SortBy.SUPPORT_REQUEST_TITLE:
+          strA = a.supportRequest.title;
+          strB = b.supportRequest.title;
+          break;
+        case SortBy.SUPPORT_REQUEST_CREATE_TIME:
+          strA = a.supportRequest.createdAt.toString();
+          strB = b.supportRequest.createdAt.toString();
+          break;
+        case SortBy.SUPPORT_REQUEST_CATEGORY:
+          strA = String(a.supportRequest.category);
+          strB = String(b.supportRequest.category);
+          break;
+        case SortBy.SUPPORT_REQUEST_STATUS:
+          strA = String(a.supportRequest.status);
+          strB = String(b.supportRequest.status);
+          break;
+        default:
+          strA = "";
+          strB = "";
+      }
+      return this.tableComparatorService.compare(by, order, strA, strB);
+    };
+  }
 }
--- src/web/app/pages-admin/admin-support-requests-page/admin-support-requests-page.module.ts
+++ src/web/app/pages-admin/admin-support-requests-page/admin-support-requests-page.module.ts
@@ -1,14 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { AjaxLoadingModule } from '../../components/ajax-loading/ajax-loading.module';
-import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
-import { AdminSupportRequestsPageComponent } from './admin-support-requests-page.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { AjaxLoadingModule } from "../../components/ajax-loading/ajax-loading.module";
+import { LoadingRetryModule } from "../../components/loading-retry/loading-retry.module";
+import { LoadingSpinnerModule } from "../../components/loading-spinner/loading-spinner.module";
+import { SupportRequestsTableModule } from "../../components/support-requests-table/support-requests-table.module";
+import { AdminSupportRequestsPageComponent } from "./admin-support-requests-page.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: AdminSupportRequestsPageComponent,
   },
 ];
@@ -17,18 +19,16 @@ const routes: Routes = [
  * Module for admin support requests page.
  */
 @NgModule({
-  declarations: [
-    AdminSupportRequestsPageComponent,
-  ],
-  exports: [
-    AdminSupportRequestsPageComponent,
-  ],
+  declarations: [AdminSupportRequestsPageComponent],
+  exports: [AdminSupportRequestsPageComponent],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild(routes),
     AjaxLoadingModule,
     LoadingSpinnerModule,
+    LoadingRetryModule,
+    SupportRequestsTableModule,
   ],
 })
-export class AdminSupportRequestsPageModule { }
+export class AdminSupportRequestsPageModule {}
